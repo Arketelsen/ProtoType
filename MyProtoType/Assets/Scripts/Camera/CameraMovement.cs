cf3cc436d3e34659ccc162b0737b4e9b0bf28fc8
Arketelsen <aketelsenb@gmail.com>
2019-09-19 10:25:50 -0500

    Cleaned Up Code

---
+++ MyProtoType/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    [SerializeField] private float _sensitivity = 1f;
+    [SerializeField]private float _minPitch, _maxPitch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _mouseY = Input.GetAxis("Mouse Y") * _sensitivity;
+        float _mouseX = Input.GetAxis("Mouse X") * _sensitivity;
+
+
+        //_mouseY = Mathf.Clamp(_mouseX, _minPitch, _maxPitch);
+
+        transform.eulerAngles = new Vector3(_mouseY, _mouseX, 0f);
+    }
+}
--- MyProtoType/Assets/Scripts/MovementController.cs
+++ MyProtoType/Assets/Scripts/MovementController.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     [SerializeField] private Transform currentTransform;
-    [SerializeField] private float speed;
-    [SerializeField] private float runSpeed;
-    [SerializeField] private float crouchSpeed;
+    [SerializeField] private float _speed, _walkSpeed, _runSpeed, _crouchSpeed;
     [SerializeField] private float jumpHeight;
     [SerializeField] private float _fallSpeed;
     [SerializeField] private bool _isCrouched = false;
@@ -49,32 +47,31 @@ public class MovementController : MonoBehaviour
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
-        float mouseX = Input.GetAxis("Mouse X");
+        //float mouseX = Input.GetAxis("Mouse X");
         
-        Vector3 inputVector = new Vector3(horizontalInput, 0, verticalInput).normalized * speed * Time.fixedDeltaTime;  //walking
-        Vector3 runVector = new Vector3(horizontalInput, 0, verticalInput).normalized * runSpeed * Time.fixedDeltaTime; //running
-        Vector3 crouchVector = new Vector3(horizontalInput, 0, verticalInput).normalized * crouchSpeed * Time.fixedDeltaTime; //crouching
+        Vector3 inputVector = new Vector3(horizontalInput, 0, verticalInput).normalized * _speed * Time.fixedDeltaTime;  //walking
         Vector3 newPostion = rb.position + rb.transform.TransformDirection(inputVector); //walking
-        Vector3 newRunPostion = rb.position + rb.transform.TransformDirection(runVector); //running
-        Vector3 newCrouchPostion = rb.position + rb.transform.TransformDirection(crouchVector); //crouching
-        Vector3 newRotation = transform.localEulerAngles;
-        newRotation.y += mouseX * _sensitivity;
-        transform.localEulerAngles = newRotation;
+        //Vector3 newRotation = transform.localEulerAngles;
+        //newRotation.y += mouseX * _sensitivity;
+        //transform.localEulerAngles = newRotation;
 
         if (Input.GetKey(KeyCode.LeftShift) && _isCrouched == false && Input.GetAxisRaw("Vertical") > 0)
         {
             //speed while standing
-            rb.MovePosition (newRunPostion);
+            _speed = _runSpeed;
+            rb.MovePosition (newPostion);
             
         }
         else if (_isCrouched == true)
         {
             //speed while crouched
-            rb.MovePosition(newCrouchPostion);
+            _speed = _crouchSpeed;
+            rb.MovePosition(newPostion);
         }
         else
         {
             //normal speed
+            _speed = _walkSpeed;
             rb.MovePosition(newPostion);
         }
 
